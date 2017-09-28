621d11333467eea0eea1905560e526560955dab6
fredericohubler <fredericohubler2@gmail.com>
2017-09-28 17:24:36 -0300

    testando commit pelo IntelliJ de novo

--- src/Conta.java
+++ src/Conta.java
@@ -15,7 +15,16 @@ public class Conta {
         listaCartoes=new ArrayList<CartaoCredito>();
     }
 
-    public String printResumo(){
+    public String Resumo(){
         return "    "+nome+"\nAgencia: "+agencia+"\nNumero da conta: "+numConta+"\nSaldo: "+saldo+"\nMensalidade: "+mensalidade+"\nDia da Mensalidade: "+diaMensalidade+"\nCartoes de credito: "+listaCartoes.size();
+    }
+
+    public void adicionaCartaoCredito(int numeroCartao, String validade, String diaFatura, String bandeira, double saldo, double limite){
+        CartaoCredito novo = new CartaoCredito(numeroCartao, validade, diaFatura, bandeira, saldo, limite);
+        listaCartoes.add(novo);
+    }
+
+    public ArrayList<CartaoCredito> getListaCartoes() {
+        return listaCartoes;
     }
 }
--- src/main.java
+++ src/main.java
@@ -8,6 +8,7 @@ public class main {
         usuarios.add(novo);
         //System.out.println(usuarios.get(0).getNome());
         usuarios.get(0).adicionaConta("0257",35205,500.37,2.99,"Banrisul", "12");
-        System.out.println(usuarios.get(0).getContas().get(0).printResumo());
+        usuarios.get(0).getContas().get(0).adicionaCartaoCredito(5544,"12/19","15","MasterCard",0.00,500.00);
+        System.out.println(usuarios.get(0).getContas().get(0).Resumo()+"\n\n"+usuarios.get(0).getContas().get(0).getListaCartoes().get(0).resumo());
     }
 }
